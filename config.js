a04f6d38ec0200471cb9c1f0ed150f812c23b971
Abhishek Sain <2021pietcaabhishek003@poornima.org>
2025-02-11 13:43:26 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const siteConfig = {
+    name: "Abhishek Sain",
+    title: "UX/UI Designer",
+    domain: "theabhisheksain.com",
+    email: "abhishek001087@gmail.com",
+    description: "UX/UI designer with a background in Engineering. Based in Jaipur, Rajasthan, India",
+    social: {
+        linkedin: "https://linkedin.com/in/abhisheksain",
+        twitter: "https://twitter.com/abhisheksain",
+    }
+};
+
+window.siteConfig = siteConfig;
